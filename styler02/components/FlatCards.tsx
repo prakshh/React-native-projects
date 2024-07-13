7bc531933320f477ac5f672ba8d97f5f53942183
prakshh <prakashdas18@gmail.com>
2024-07-13 16:35:50 +0530

    flex, card, cardOne

--- styler02/components/FlatCards.tsx
+++ styler02/components/FlatCards.tsx
@@ -4,9 +4,35 @@ import React from 'react'
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    container: {
+
+    },
+    card: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100, 
+        borderRadius: 4,
+        margin: 8
+    },
+    cardOne: {
+        backgroundColor: '#EF5354'
+    }
+})
