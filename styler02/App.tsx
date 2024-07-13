3d2ef4f4e9569473b677e84f1c65a98f99d58110
prakshh <prakashdas18@gmail.com>
2024-07-13 14:51:05 +0530

    app2 - start

---
+++ styler02/App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>App</Text>
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
