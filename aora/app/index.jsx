870ebc01082315530f01ec7daf1da75c3d7ef636
prakshh <prakashdas18@gmail.com>
2024-09-13 14:16:59 +0530

    assets, snippets for tailwind, Font Loaded, useEffect

--- aora/app/index.jsx
+++ aora/app/index.jsx
@@ -4,8 +4,8 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-yellow">
-      <Text className="text-3xl">Aora!!!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!!!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
