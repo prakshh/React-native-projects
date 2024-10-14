9d8c62963bb7c75c72aa31b1e032b1030c550ac6
prakshh <prakashdas18@gmail.com>
2024-10-14 18:12:39 +0530

    auth screen - sign-in

--- aora/app/index.jsx
+++ aora/app/index.jsx
@@ -1,6 +1,6 @@
 import { Image, ScrollView, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar'; 
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from '../constants';
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -42,7 +42,7 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}} 
+            handlePress={() => router.push('/sign-in')} 
             containerStyles="w-full mt-7"
           />
         </View>
