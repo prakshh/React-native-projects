c169f83d21094204a90d4644130f05c28031bd66
prakshh <prakashdas18@gmail.com>
2024-11-14 01:49:54 +0530

    homepage - emptyState - custom display when data is empty

--- aora/app/(tabs)/home.jsx
+++ aora/app/(tabs)/home.jsx
@@ -5,12 +5,14 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, {id: 3},]}
+        // data={[{ id: 1 }, { id: 2 }, {id: 3},]}
+        data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
@@ -36,6 +38,14 @@ const Home = () => {
               <Trending posts={[{ id: 1}, {id: 2}, {id: 3}] ?? []}/>
             </View>
           </View>
+        )}
+        ListEmptyComponent={() => (
+          // <Text className="text-white">Empty</Text>
+          // data={[]} - if data is empty, then ListEmptyComponent will display "Empty"
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
         )}
       />
     </SafeAreaView>
